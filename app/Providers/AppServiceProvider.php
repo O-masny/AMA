0f9e1f822d162d07824876db9521e83983a95fed
ondrej masny <iicelyy@seznam.cz>
2025-08-20 13:41:35 +0200

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <- tady je klíčové
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,12 +20,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->environment('production')) {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
 
         // Alternativně můžete použít:
         if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 
