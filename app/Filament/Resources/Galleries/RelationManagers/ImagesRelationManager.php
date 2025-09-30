e6e7612eb2839fbf69202083acdfa587425f2715
ondrej masny <iicelyy@seznam.cz>
2025-09-30 17:17:30 +0200

    fx

--- app/Filament/Resources/Galleries/RelationManagers/ImagesRelationManager.php
+++ app/Filament/Resources/Galleries/RelationManagers/ImagesRelationManager.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filament\Resources\Exhibitions\RelationManagers;
+namespace App\Filament\Resources\Galleries\RelationManagers;
 
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
