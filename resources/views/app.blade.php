bbc9b06499280152f4eda55565486bb5a8ed8754
ondrej masny <iicelyy@seznam.cz>
2025-10-01 13:26:57 +0200

    FX

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -40,12 +40,9 @@
         }
     </style>
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'AMatelier') }}</title>
 
     {{-- Favicon --}}
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
 
     {{-- Ziggy routes --}}
